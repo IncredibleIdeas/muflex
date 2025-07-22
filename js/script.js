12072984effce490fb908ade861cd2f84451933a
IncredibleIdeas <opokukwadwo642@gmail.com>
2025-07-22 19:46:15 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function openNav() {
+	w3.show(".nav");
+	w3.show("#close-btn");
+	w3.hide("#menu-btn");
+}
+
+function closeNav() {
+	w3.hide(".nav");
+	w3.hide("#close-btn");
+	w3.show("#menu-btn");
+}
+
